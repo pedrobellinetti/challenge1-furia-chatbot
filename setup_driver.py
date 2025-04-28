31c87e4602327303766b5d5208d407ddca642fea
pedrobellinetti <pedrobellinetti9@gmail.com>
2025-04-28 19:27:32 -0300

    Caminho para chromedriver.exe consertado

--- setup_driver.py
+++ setup_driver.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 import os
 
 # Configurações do chromedriver
-chromedriver_path = "/mnt/e/chromedriver/chromedriver.exe"
+chrome_driver_path = "E:\\chromedriver\\chromedriver-win64\\chromedriver.exe" # Onde está instalado o chromedriver
 
 # Token do bot
 load_dotenv()
