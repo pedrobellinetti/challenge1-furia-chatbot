1fc8a25b9633565466fb2dffad36bc1a74efb743
pedrobellinetti <pedrobellinetti9@gmail.com>
2025-04-28 18:59:47 -0300

    Função de histórico de partidas

--- funcoes_busca_jogos.py
+++ funcoes_busca_jogos.py
@@ -9,7 +9,7 @@ from time import sleep
 import random
 import os
 from dotenv import load_dotenv
-import setup_driver
+from setup_driver import *
 
 # Scrapper próximas partidas
 
@@ -21,5 +21,49 @@ def proximos_jogos():
     matches = []
     
     try:
-        upcoming_section = driver.find_element()
+        upcoming_section = driver.find_element(By.ID, "upcomingMatchesBox")
+        upcoming_rows = upcoming_section.find_elements(By.CLASS_NAME, "match")
+        for row in upcoming_rows:
+            date = row.find_element(By.CLASS_NAME, "matchTime").text
+            teams = row.find_elements(By.CLASS_NAME, "matchTeamName")
+            bo_format = row.find_element(By.CLASS_NAME, "matchMeta").text
+            
+            if len(teams) == 2:
+                team1 = teams[0].text
+                team2 = teams[1].text
+                matches.append(f"📆 {date} - {team1} vs {team2} ({bo_format})")
+    except Exception as e:
+        print("Erro ao buscar partidas futuras: ", e)
+        
+    driver.quit()
+    return matches
+
+def historico_jogos():
+    driver = setup_driver()
+    driver.get("https://www.hltv.org/team/8297/furia#tab-matchesBox")
+    sleep(random.uniform(2, 4))
+    
+    results = []
+    try:
+        past_section = driver.find_element(By.ID, "pastMatchesBox")
+        past_rows = past_section.find_elements(By.CLASS_NAME, "result-con")
+        
+        for row in past_rows:
+            date = row.find_element(By.CLASS_NAME, "time").text
+            teams = row.find_elements(By.CLASS_NAME, "team")
+            score = row.find_element(By.CLASS_NAME, "result-score").text    
+            bo_format = row.find_element(By.CLASS_NAME, "map-text").text
+            
+            if len(teams) == 2:
+                team1 = teams[0].text
+                team2 = teams[1].text
+                results.append(f"📆 {date} - {team1} {score} vs {team2} {score} ({bo_format})")
+    except Exception as e:
+        print(f"Erro ao buscar partidas passadas: {e}")
+        
+    driver.quit()
+    return results
+
+
+                
 
