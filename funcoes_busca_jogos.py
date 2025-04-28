baab044ca5a1b58b0294efb37fba4619be7b0f49
pedrobellinetti <pedrobellinetti9@gmail.com>
2025-04-28 18:51:42 -0300

    Função para buscar jogos futuros

--- funcoes_busca_jogos.py
+++ funcoes_busca_jogos.py
@@ -9,4 +9,17 @@ from time import sleep
 import random
 import os
 from dotenv import load_dotenv
+import setup_driver
+
+# Scrapper próximas partidas
+
+def proximos_jogos():
+    driver = setup_driver()
+    driver.get("https://www.hltv.org/team/8297/furia#tab-matchesBox")
+    sleep(random.uniform(2, 4))
+    
+    matches = []
+    
+    try:
+        upcoming_section = driver.find_element()
 
